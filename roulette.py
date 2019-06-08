dd77db54f2f111af04155fda44c4d39447bb6398
Kaleiko <Chase@kaleiko.net>
2019-06-08 15:57:45 -0700

    started dictionary

---
+++ roulette.py
@@ -0,0 +1,18 @@
+# American Roulette with 0 and 00
+
+
+roulette_number{
+    1:['1', 'RED', 'ODD', '1 TO 18', '1 TO 12', '1ST 12'],
+    2:['2', 'BLACK', 'EVEN', '1 TO 18' '1 TO 12', '2ND 12'],
+    3:['3', 'RED', 'ODD', '1 TO 18', '1 TO 12', '3RD 12'],
+    4:['4', 'BLACK', 'EVEN', '1 TO 18', '1 TO 12', '1ST 12'],
+    5:['5', 'RED', 'ODD', '1 TO 18', '1 TO 12', '2ND 12'],
+    6:['6', 'BLACK', 'EVEN', '1 TO 18', '1 TO 12', '3RD 12'],
+    7:['7', 'RED', 'ODD', '1 TO 18', '1 TO 12', '1ST 12'],
+    8:['8', 'BLACK', 'EVEN', '1 TO 18', '1 TO 12', '2ND 12'],
+    9:['9', 'RED', 'ODD', '1 TO 18', '1 TO 12', '3RD 12'],
+    10:['10', 'BLACK', 'EVEN', '1 TO 18', '1 TO 12', '1ST 12'],
+    11:['11', 'BLACK', 'ODD', '1 TO 18', '1 TO 12', '2ND 12'],
+    12:['12', 'RED', 'EVEN', '1 TO 18', '1 TO 12', '3RD 12'],
+
+}
